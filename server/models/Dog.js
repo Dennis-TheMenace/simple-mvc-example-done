d1cb21c0d944e1eea9a124e8412194d61b35d28f
Alex Gray <arg2399@rit.edu>
2022-03-21 17:40:11 -0400

    Working on simple models assignment

---
+++ server/models/Dog.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+let DogModel = {};
+
+//Name, # of beds, createdDate
+const DogSchema = new mongoose.Schema(
+{
+    name: 
+    {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    breed:
+    {
+        type: String,
+        required: true,
+    },
+    age:
+    {
+        type: Number,
+        min: 0,
+        required: true,
+
+    },
+    createdDate:
+    {
+        type: Date,
+        default: Date.now,
+
+    },
+});
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports = DogModel;
